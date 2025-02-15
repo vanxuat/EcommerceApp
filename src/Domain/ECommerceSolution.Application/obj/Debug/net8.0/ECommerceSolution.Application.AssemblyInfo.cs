4bb6b0726c45deb08fcd68d4b22e4780277f97e7
vanxuat <xuat0986782902@gmail.com>
2025-02-15 15:36:15 +0700

    Build struct application layer

--- src/Domain/ECommerceSolution.Application/obj/Debug/net8.0/ECommerceSolution.Application.AssemblyInfo.cs
+++ src/Domain/ECommerceSolution.Application/obj/Debug/net8.0/ECommerceSolution.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceSolution.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1583ebd8cff954a2322e4656e25285604fb0fda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df3e9041bfa4db37e6228ad858e590f2e480fefb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceSolution.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceSolution.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Domain/ECommerceSolution.Data/obj/Debug/net8.0/ECommerceSolution.Data.AssemblyInfo.cs
+++ src/Domain/ECommerceSolution.Data/obj/Debug/net8.0/ECommerceSolution.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceSolution.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1583ebd8cff954a2322e4656e25285604fb0fda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df3e9041bfa4db37e6228ad858e590f2e480fefb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceSolution.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceSolution.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
